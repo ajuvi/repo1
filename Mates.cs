ba6123cb7aa16c0a07e7fee69ab96203b1526bb3
ajuvi <102821845+ajuvi@users.noreply.github.com>
2025-03-26 16:52:10 +0100

    Suma

--- Mates.cs
+++ Mates.cs
@@ -1,6 +1,16 @@
 class Mates{
 
     public static int Suma(int[] nums){
+            int sum;
+
+        for(int i=0;i<nums.Length;i++){
+
+            sum=sum+nums[0];
+
+        }
+
+
+        return sum;
     }
 
     public static int Maxim(int[] nums){
